e85121922342e204fd104c7eba74f6ba155c6a73
miutamihai <miuta.mihai@gmail.com>
2023-05-29 14:05:11 +0300

    [master] Add mock footer and header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.css";
+    import Header from '$lib/header/index.svelte'
+    import Footer from '$lib/footer/index.svelte'
 </script>
 
+<Header />
 <slot />
+<Footer/>
