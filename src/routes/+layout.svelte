1f9ef07b1b66aa70883f33b497e2ce63b3a38477
radu <radu.buciuceanu@gmail.com>
2023-05-29 18:30:46 +0300

    [mihaiandradu] Adjust styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
     import Footer from '$lib/footer/index.svelte'
 </script>
 
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</svelte:head>
+
 <div class="page">
     <Header />
     <slot />
@@ -14,5 +18,9 @@
 <style>
     .page {
         background: linear-gradient(108.43deg, #0F1A2B 0.28%, #0F1A2B 100%);
+    }
+
+    div {
+        font-family: "Montserrat";
     }
 </style>
