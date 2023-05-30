1c644a9428358e02643c3ec8c8eda062faab27d9
miutamihai <miuta.mihai@gmail.com>
2023-05-30 12:15:06 +0300

    [master] Add some styling to meet the nerds

---
+++ src/lib/home/meet/hashtag.svelte
@@ -0,0 +1,3 @@
+<div class="rounded-lg shadow-md bg-primary flex flex-col px-8 py-4 bg-opacity-30 border-2 border-gray-600 border-opacity-30 backdrop-filter backdrop-blur-sm text-blue-500">
+    <slot/>
+</div>
--- src/lib/home/meet/index.svelte
+++ src/lib/home/meet/index.svelte
@@ -1,13 +1,37 @@
 <script>
     import CurliedText from '$lib/components/curlied-text.svelte'
+    import HashTag from './hashtag.svelte'
 </script>
 
-<div class="w-full flex flex-col">
-    <CurliedText braceColor="text-white" textColor="text-blue">
+<div class="w-full flex flex-col mb-20">
+    <CurliedText braceColor="text-white" textColor="text-blue-500">
         about us
     </CurliedText>
 
-    <div class="text-white">
-        Meet the Nerds
+    <div class="grid grid-cols-2 gap-3">
+        <div class="text-5xl text-white font-bold mb-5">
+            Meet the Nerds
+        </div>
+        <div class="flex items-center justify-evenly">
+            <HashTag>
+                #software development
+            </HashTag>
+            <HashTag>
+                #tech-savy
+            </HashTag>
+            <HashTag>
+                #polyglots
+            </HashTag>
+        </div>
+        <div class="text-white">
+            We are a team of polyglots which are specialized in a wide range of technologies.
+            The one thing we have in common is that we’re always ready to learn something new.
+            This is what technology is all about – chasing the newest way to serve people better.
+        </div>
+        <div class="text-white">
+            Staying true to our authenticity is important to us because, with this authenticity,
+            we have constructed our common unity, represented by the space where we can develop and grow as
+            individuals together and reflect it into our code, our way of working, and our way of life.
+        </div>
     </div>
 </div>
